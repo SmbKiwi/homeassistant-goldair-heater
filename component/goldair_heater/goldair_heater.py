da1c7169d125da1ea02f7e75e09f0558583723f3
Shane <47836318+SmbKiwi@users.noreply.github.com>
2019-09-17 17:05:08 +1200

    Update goldair_heater.py

--- component/goldair_heater/goldair_heater.py
+++ component/goldair_heater/goldair_heater.py
@@ -3,6 +3,12 @@ Platform for Goldair WiFi-connected heaters and panels.
 Based on sean6541/tuya-homeassistant for service call logic, and TarxBoy's
 investigation into Goldair's tuyapi statuses
 https://github.com/codetheweb/tuyapi/issues/31.
+
+Version 2.0 Author: SmbKiwi
+17 September 2019
+Updated for HA Climate 1.0 (HA 0.96+)
+Based on https://github.com/nikrolls/homeassistant-goldair-climate/tree/0.0.1
+
 """
 from time import time
 from threading import Timer, Lock
